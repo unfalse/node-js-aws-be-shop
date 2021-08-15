470c0c5ca66b7801f46e9998cb9d0b1ef271f870
Iskander Gubaidullin <Iskander_Gubaidullin@epam.com>
2021-08-15 20:02:34 +0500

    added unit-test for importProductsFile
    Task: 5

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,13 +1,12 @@
 import 'source-map-support/register';
 
-import { APIGatewayProxyHandler } from 'aws-lambda';
 import AWS from 'aws-sdk';
 
 import { formatResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
 import { STATUS_CODES, BUCKET } from '@libs/const';
 
-export const importProductsFile: APIGatewayProxyHandler = async (event) => {
+export const importProductsFile = async (event) => {
     console.log('importProductsFile lambda is executing', {
         method: event.httpMethod,
         pathParameters: event.pathParameters,
@@ -16,16 +15,21 @@ export const importProductsFile: APIGatewayProxyHandler = async (event) => {
     });
 
     const { name } = event.queryStringParameters;
+    
     const s3 = new AWS.S3({ region: 'eu-west-1' });
+    
     const params = {
         Bucket: BUCKET,
         Key: 'uploaded/' + name,
         Expires: 60,
         ContentType: 'text/csv'
     };
-    const signedURL = await s3.getSignedUrlPromise('putObject', params);
 
-    return formatResponse(signedURL, STATUS_CODES.OK, false);
+    return formatResponse(
+        await s3.getSignedUrlPromise('putObject', params),
+        STATUS_CODES.OK,
+        false
+    );
 }
 
 export const main = middyfy(importProductsFile);
---
+++ import-service/src/tests/importProductsFile.test.ts
@@ -0,0 +1,30 @@
+import AWSMock from 'aws-sdk-mock';
+import AWS from 'aws-sdk';
+
+import { BUCKET } from '@libs/const';
+import { importProductsFile } from 'src/functions/importProductsFile/handler';
+
+describe('importProductsFile:', () => {
+  it('should return url', async() => {
+    const urlMock = ({ Bucket, Key, Expires, ContentType }) => `https://amazonaws.com/getObject?key=${Key}&bucket=${Bucket}&ContentType=${ContentType}&Expires=${Expires}`;
+
+    AWSMock.setSDKInstance(AWS);
+
+    AWSMock.mock('S3', 'getSignedUrl', async (operation: string, params: any, callback: Function) => {
+      console.log('S3', 'getSignedUrl', 'mock called with parameters: ', {operation, params});
+      callback(null, urlMock(params));
+    });
+
+    const params = {
+      Bucket: BUCKET,
+      Key: 'uploaded/productsmock.csv',
+      Expires: 60,
+      ContentType: 'text/csv'
+    };
+
+    const response = await importProductsFile({queryStringParameters: { name: 'productsmock.csv' }});
+    expect(response.body).toBe(urlMock(params));
+
+    AWSMock.restore('S3');
+  });
+});
